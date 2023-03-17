956e5430344f2e3e01386979f1b7fc27eb61860b
Christophe.FONSECA-DIOGO <Christophe.FONSECA-DIOGO@cpnv.ch>
2023-03-17 09:18:20 +0100

    Number of Movement done And Commentary

--- 2048.py
+++ 2048.py
@@ -1,5 +1,5 @@
 #####################################
-# Project 2048 V.02                  #
+# Project 2048 V.02                 #
 # Date : 26.01.2023                 #
 # Made by Christophe Fonseca Diogo  #
 #####################################
@@ -25,25 +25,42 @@ window.geometry("+{}+{}".format(x_left, y_top))
 #########################################
 # Variables                             #
 #########################################
-
 movement = 0
-
 
 #########################################
 # Functions                             #
 #########################################
-
-def Mix(list, rev):
+def deleteZeros(list,rev):
+    global movement
+    if rev == False:
+        i = len(list) - 1
+        while i != 0 and list[i] == 0:
+            movement -= 1
+            i -= 1
+    else:
+        i = 0
+        while i < len(list) and list[i] == 0:
+            movement -= 1
+            i += 1
     for obj in list:
         if 0 in list:
             list.remove(0)
+            movement += 1
+
+#########################################
+# Function for mix the grid             #
+#########################################
+def Mix(list, rev):
+    global movement
+    movement = 0
+
+    deleteZeros(list,rev)
     for obj in range(len(list) - 1):
         if list[obj] == list[obj + 1]:
             list[obj] += list[obj + 1]
             list[obj + 1] = 0
-    for obj in list:
-        if 0 in list:
-            list.remove(0)
+            movement += 1
+    deleteZeros(list,rev)
     if rev:
         totalNumbers = 4 -len(list)
         while totalNumbers != 0:
@@ -52,28 +69,42 @@ def Mix(list, rev):
     else:
         while len(list) < 4:
             list.append(0)
+    # If the movement < 0
+    if movement < 0:
+    # the movement gonna be 0
+        movement = 0
+    print(f"nb mouvement : {movement}")
     return list
 
 
+
 #########################################
+# Functions when the user presses a keybind
 #########################################
 
 def click_on_letter(event):
+    # If the user presses the Left key or the key "a" the grid will mix to the Left
     if event.keysym == 'Left' or event.keysym == 'a':
         for line in range(len(grid_2048)):
             grid_2048[line] = Mix(grid_2048[line], False)
+        # Refresh the grid
         objRefresh()
+    # If the user presses the Right key or the key "d" the grid will mix to the Left
     elif event.keysym == 'Right' or event.keysym == 'd':
         for line in range(len(grid_2048)):
             grid_2048[line] = Mix(grid_2048[line], True)
+        # Refresh the grid
         objRefresh()
+    # If the user presses the Up key or the key "w" the grid will mix to the Left
     elif event.keysym == 'Up' or event.keysym == 'w':
         for col in range(len(grid_2048)):
             column = [grid_2048[line][col] for line in range(len(grid_2048))]
             mix_column = Mix(column,False)
             for line in range(len(grid_2048)):
                 grid_2048[line][col] = mix_column[line]
+        # Refresh the grid
         objRefresh()
+    # If the user presses the Down key or the key "s" the grid will mix to the Left
     elif event.keysym == 'Down' or event.keysym == 's':
         for col in range(len(grid_2048)):
             column = [grid_2048[line][col] for line in range(len(grid_2048))]
@@ -81,6 +112,7 @@ def click_on_letter(event):
             mix_column = Mix(reversed_column,False)
             for line in range(len(grid_2048)):
                 grid_2048[line][col] = mix_column[::-1][line]
+        # Refresh the grid
         objRefresh()
 
 
@@ -88,7 +120,7 @@ def click_on_letter(event):
 # Table for the grid                    #
 #########################################
 
-grid_2048= [[2, 2, 2, 2], [4, 0, 128, 0], [256, 32, 2048, 8], [64, 512, 16, 1024]]
+grid_2048= [[0, 0, 0, 4], [4, 0, 0, 2], [2, 2, 2, 4], [2, 2, 0, 0]]
 labels = [[None, None, None, None], [None, None, None, None], [None, None, None, None],[None, None, None, None]]
 
 #########################################
@@ -118,6 +150,7 @@ if __name__ == '__main__':
                 labels[line][col].grid(row=line, column=col)
                 try:labels[line][col].config(bg=colors_bg[int(grid_2048[line][col])])
                 except:labels[line][col].config(bg="Yellow")
+    # Refresh the grid
     objRefresh()
 window.bind('<Key>', click_on_letter)
 
